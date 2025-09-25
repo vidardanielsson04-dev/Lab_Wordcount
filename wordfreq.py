cdaa0ecc1be6c2daf316231d2f82a134b295b7b0
Elias0114 <elias.hamlet@gmail.com>
2025-09-25 10:59:51 +0200

    Add files via upload

--- wordfreq.py
+++ wordfreq.py
@@ -1,8 +1,8 @@
 # = förklaring av korden
 ### = förslag på ändring / tankar kring korden
-document = ['''Francis Albert Sinatra, nicknamed the "Chairman of the Board" and "Ol' Blue Eyes", was an American singer and actor. He is regarded as one of the most popular entertainers of the 20th century. He is among the world's best-selling music artists, with an estimated 150 million record sales globally.''']
+document = ['''x-ray Francis Albert Sinatra, nicknamed the "Chairman of the Board" and "Ol' Blue Eyes", was an American singer and actor. He is regarded as one of the most popular entertainers of the 20th century. He is among the world's best-selling music artists, with an estimated 150 million record sales globally.''']
 words = []
-def tokenize(document):
+def tokenize(document): #metod för att definera vad som är ett ord och skapa en lista med alla ord
     for line in document:
         line = line.lower()
         i = 0
@@ -11,7 +11,7 @@ def tokenize(document):
             if ch.isalpha() or ch.isdigit():
                 token = ch
                 i += 1
-                while i < len(line) and (line[i].isalpha() or line[i].isdigit() or (line[i] == "'" and line[i+1].isalpha())): #om bokstav, om nummer eller om ' följt av bokstav
+                while i < len(line) and (line[i].isalpha() or line[i].isdigit() or (line[i] == "'" and line[i+1].isalpha()) or (line[i] == "-" and line[i+1].isalpha())): #om bokstav, om nummer eller om ' eller - följt av bokstav, så att även don't och x-ray räknas som ord
                     token += line[i]
                     i += 1
                 words.append(token)
@@ -20,13 +20,13 @@ def tokenize(document):
     return words
 
 
-def countWord(words):
+def countWord(words): #metod för att räkna och sortera orden
     tokenize(document)
     i = 0
     n = 0
     nyLista = []
     vardeLista = []
-    slutLista = [] ###går det att göra med mindre antal listor?
+    slutDic = {} #skapar en dictionary
     while i < len(words):
         if words[i] not in nyLista: #om nytt ord >
             nyLista.append(words[i]) #lägg till ord i ny lista
@@ -37,8 +37,15 @@ def countWord(words):
             ###finns count funktion som kanske hade varit snyggare
         i += 1
     i = 0
-    while i < len(nyLista):
-        slutLista.append(str(nyLista[i]) + ' : ' + str(vardeLista[i])) #lägger ihop orden med dess värden   ###nested list istället?
-        i += 1
-    return slutLista
-print(countWord(words))
+    while i < len(nyLista): #lägger ihop ord och värde listan i en gemensam dictionary
+        slutDic[nyLista[i]] = vardeLista[i] #ord [i] får värdet av värdelista[i]
+        i += 1 
+    sorteradDic = sorted(slutDic.items(), key=lambda x:x[1], reverse=True) #sorterar dictionary, (x:x[1]) innebär att vi sorterar efter det andra elementet dvs int, x:x[0] hade sorterat efter bokstav istället.
+    return sorteradDic #reverse=True ovan är för att den annars sorterar efter minsta värde först
+
+
+### vi bör kanske sortera efter bokstav på de ord som finns lika många gånger, fråga läraren. isåfall förslagsvis i första metoden alltså innan counting
+### vi behöver också ta bort ord "stopwords" förslagsvis vid början av counting
+### fråga om vi behöver ha med nummer som ord
+
+### sedan ska vi skapa en snyggare print i en ny metod
